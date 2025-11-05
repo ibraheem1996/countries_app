f273d054a648af6b65b56182d96ccf02de9c0fcd
ibraheem <abraheemalhen@gmail.com>
2025-11-05 21:06:23 +0300

    Creating a home screen page

--- lib/features/home/ui/home.dart
+++ lib/features/home/ui/home.dart
@@ -1,10 +1,70 @@
+import 'package:countries/features/home/ui/widget/grid_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:countries/features/home/logic/home_cubit.dart';
+import 'package:countries/features/home/data/model/modele.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<HomeCubit>().getHomeData();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Countries 🌍"), centerTitle: true),
+      body: BlocConsumer<HomeCubit, HomeState>(
+        listener: (context, state) {
+          state.mapOrNull(
+            error: (e) {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text(e.failure.message)));
+            },
+          );
+        },
+        builder: (context, state) {
+          if (state is Loading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (state is Loaded) {
+            final List<HomeModele> countries = state.modeles;
+
+            return GridViewBuilder(countries: countries);
+          }
+
+          if (state is Error) {
+            return Center(
+              child: Text(
+                state.failure.message,
+                style: const TextStyle(color: Colors.red, fontSize: 18),
+              ),
+            );
+          }
+
+          return const Center(
+            child: Text(
+              "Press the refresh button to load countries 🌎",
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.read<HomeCubit>().getHomeData(),
+        child: const Icon(Icons.refresh),
+      ),
+    );
   }
 }
