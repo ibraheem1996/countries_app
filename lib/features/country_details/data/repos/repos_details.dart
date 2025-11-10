d28bccfb72aca41b75eea51f751606e4bff5abdd
ibraheem <abraheemalhen@gmail.com>
2025-11-10 19:46:10 +0300

    1-add to json details model 2- add repositoryImpl

--- lib/features/country_details/data/repos/repos_details.dart
+++ lib/features/country_details/data/repos/repos_details.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:countries/features/country_details/data/data_sourse/local_details_data_sourse.dart';
 import 'package:countries/features/country_details/data/model/details_model.dart';
 
 import '../../../../networking/api_result.dart';
@@ -7,16 +10,27 @@ import '../data_sourse/remote_details_data_sourse.dart';
 
 class DetailsReposImpl implements DetailsRepository {
   final RemoteDetailsDataSourse remoteDetailsDataSourse;
+  final LocalDetailsDataSourse localDetailsDataSourse;
 
-  DetailsReposImpl({required this. remoteDetailsDataSourse});
+  DetailsReposImpl({required this. remoteDetailsDataSourse , required this.localDetailsDataSourse});
 
   @override
   Future<ApiResult<List<DetailsModel>>> getDetailsData(String cca3) async {
+    final cached = localDetailsDataSourse.getDetails(theKey: cca3);
     try {
       final result = await remoteDetailsDataSourse.detailsCountry(cca3);
-      
-      return ApiResult.success(result);
+      final json = result.map((e) => e.toJson()).toList();
+      await localDetailsDataSourse.save(detailsModel: jsonEncode(json), theKey: cca3);
+      final entities = result.map((e)=> e.toEntity()).toList();
+      return ApiResult.success(entities);
     } catch (e) {
+        if ( cached != null && cached.isNotEmpty) {
+          final List decoded = jsonDecode(cached);
+          final cachedModels = decoded.map<DetailsModel>((e) => DetailsModel.fromJson(e)).toList();
+      final entities = cachedModels.map((e) => e.toEntity()).toList();
+
+          return ApiResult.success(entities);
+        }
       return ApiResult.error(ErrorHandler.handle(e));
     }
   }
