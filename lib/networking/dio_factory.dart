840255a7a037f319194e38818a5c8a34c098c9ff
ibraheem <abraheemalhen@gmail.com>
2025-11-11 16:30:31 +0300

    edit cubit add closed fun

--- lib/networking/dio_factory.dart
+++ lib/networking/dio_factory.dart
@@ -15,8 +15,8 @@ class DioFactory {
 
     final options = BaseOptions(
       baseUrl: ApiConstants.baseUrl,
-      connectTimeout: const Duration(seconds: 30),
-      receiveTimeout: const Duration(seconds: 30),
+      connectTimeout: const Duration(seconds: 15),
+      receiveTimeout: const Duration(seconds: 15),
       // headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
     );
 
